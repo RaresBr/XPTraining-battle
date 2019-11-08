bd2858bc1678e06c821472f5158fe6c89eee7c82
Rares Bradea <rares.bradea@cegeka.com>
2019-11-08 14:46:27 +0200

    us6-add console log to i headquarters

---
+++ Battle/Headquarters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Battle
+{
+    public class Headquarters : IHeadquarters
+    {
+        private int _soldierId;
+
+        public int ReportEnlistment(string soldierName)
+        {
+            return _soldierId++;
+        }
+
+        public void ReportCasualty(int soldierId)
+        {
+            Console.Write(soldierId);
+        }
+
+        public void ReportVictory(int remainingNumberOfSoldiers)
+        {
+            Console.Write(remainingNumberOfSoldiers);
+        }
+    }
+}
